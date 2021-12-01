c4f28f7455f86bd29303148ae81e8b4142d94510
ZacharyWin <2816813070@qq.com>
2021-12-01 19:15:08 +0800

    feat(service): template-instance

--- src/service/template-instance.ts
+++ src/service/template-instance.ts
@@ -10,6 +10,8 @@ import {
 import { loadDocxFile } from '../utils/doc'
 import { readFileSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
+import { FileStream } from '../../typings/app'
+import xlsx from 'xlsx'
 
 export enum TemplateType {
   DOCX,
@@ -28,37 +30,53 @@ export class TemplateInstanceService {
     tags,
     staffId,
     type,
-  }: NewTemplateInstanceDTO): Promise<boolean> {
-    const template = await this.templateModel.findOne(templateId)
-    if (!template) {
-      return false
-    }
+  }: NewTemplateInstanceDTO): Promise<[boolean, any]> {
+    try {
+      const template = await this.templateModel.findOne(templateId)
+      if (!template) {
+        return [false, new Error('template not found')]
+      }
 
-    let instance = new TemplateInstance()
-    instance = {
-      ...instance,
-      type,
-      templateId,
-      staffId,
-      tags: JSON.stringify(tags),
+      let instance = new TemplateInstance()
+      instance = {
+        ...instance,
+        type,
+        templateId,
+        staffId,
+        tags: JSON.stringify(tags),
+      }
+
+      await this.templateInstanceModel.insert(
+        this.templateInstanceModel.create(instance)
+      )
+      return [true, null]
+    } catch (e: any) {
+      return [false, e]
     }
-    await this.templateInstanceModel.create(instance)
-    return true
   }
 
-  async delInstance(instanceId: string): Promise<true> {
-    await this.templateInstanceModel.delete(instanceId)
+  async delInstance(instanceId: string): Promise<boolean> {
+    try {
+      await this.templateInstanceModel.delete(instanceId)
+    } catch (e: any) {
+      console.log(e.message)
+      return false
+    }
     return true
   }
 
   async updateInstance({
     id: instanceId,
     tags,
-  }: UpdateTemplateInstanceDTO): Promise<true> {
-    await this.templateInstanceModel.update(instanceId, {
-      tags: JSON.stringify(tags),
-    })
-    return true
+  }: UpdateTemplateInstanceDTO): Promise<[boolean, any]> {
+    try {
+      await this.templateInstanceModel.update(instanceId, {
+        tags: JSON.stringify(tags),
+      })
+    } catch (e: any) {
+      return [false, e]
+    }
+    return [true, null]
   }
 
   async queryInstance(instanceId: string): Promise<TemplateInstance> {
@@ -71,25 +89,34 @@ export class TemplateInstanceService {
       where: {
         staffId,
       },
+      select: ['id', 'templateId', 'type'],
     })
     return instances
   }
 
-  async copyInstance(instanceId: string): Promise<string> {
+  async copyInstance(instanceId: string): Promise<[boolean, any]> {
     const instance = await this.templateInstanceModel.findOne(instanceId)
-    const newInstance = await this.templateInstanceModel.create({
+    if (!instance) return [false, new Error('instance not found')]
+    const newInstance = this.templateInstanceModel.create({
       ...instance,
-      id: null,
     })
-    return newInstance.id
+
+    delete newInstance['id']
+
+    try {
+      await this.templateInstanceModel.insert(newInstance)
+    } catch (e: any) {
+      return [false, e]
+    }
+    return [true, newInstance.id]
   }
 
-  async downloadInstance(instanceId: string): Promise<[boolean, string]> {
+  async downloadInstance(instanceId: string): Promise<[boolean, any]> {
     const instance = await this.templateInstanceModel.findOne(instanceId)
-    if (!instance) return [false, null]
+    if (!instance) return [false, new Error('instance not found')]
     const { templateId } = instance
     const template = await this.templateModel.findOne(templateId)
-    if (!templateId) return [false, null]
+    if (!templateId) return [false, new Error('template not found')]
 
     const doc = await loadDocxFile(readFileSync(template.path, 'binary'))
     doc.render(instance.tags)
@@ -99,6 +126,37 @@ export class TemplateInstanceService {
     })
 
     const tmpPath = resolve('temp', template.filename)
+    writeFileSync(tmpPath, content)
+
+    return [true, tmpPath]
+  }
+
+  async uploadExcelAndSaveToInstance(
+    fid: string,
+    file: FileStream
+  ): Promise<[boolean, string]> {
+    const template = await this.templateModel.findOne({
+      where: {
+        fid,
+        type: TemplateType.EXCEL,
+      },
+    })
+
+    if (!template) return [false, null]
+
+    const doc = loadDocxFile(readFileSync(template.path, 'binary'))
+    const workbook = xlsx.read(file)
+    const data = xlsx.utils.sheet_to_json(
+      workbook.Sheets[workbook.SheetNames[0]]
+    )
+
+    doc.render(data)
+    const content = doc.getZip().generate({
+      type: 'nodebuffer',
+    })
+
+    const tmpPath = resolve('temp', template.filename)
+
     writeFileSync(tmpPath, content)
 
     return [true, tmpPath]
