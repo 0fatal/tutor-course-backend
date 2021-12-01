6b5eab3790cccca9c683993e029da60eb1d42679
ZacharyWin <2816813070@qq.com>
2021-12-01 19:14:43 +0800

    feat(controller): template

--- src/controller/template.ts
+++ src/controller/template.ts
@@ -9,24 +9,24 @@ import {
   Post,
   Provide,
   Query,
-} from '@midwayjs/decorator';
-import { Context } from '@midwayjs/web';
-import { createReadStream, statSync } from 'fs';
-import { R } from '../utils/response';
-import { TemplateService } from '../service/template';
+} from '@midwayjs/decorator'
+import { Context } from '@midwayjs/web'
+import { createReadStream, statSync } from 'fs'
+import { R } from '../utils/response'
+import { TemplateService } from '../service/template'
 
 @Provide()
 @Controller('/template')
 export class TemplateController {
   @Inject()
-  _templateService: TemplateService;
+  _templateService: TemplateService
 
   @Post('/parse')
   async parse(ctx: Context): Promise<R> {
-    const file = await ctx.getFileStream();
-    const [ifSuccess, res] = await this._templateService.parseTemplate(file);
+    const file = await ctx.getFileStream()
+    const [ifSuccess, res] = await this._templateService.uploadTemplate(file)
 
-    return ifSuccess ? R.Ok().Data(res) : R.Fail().Msg(res.message);
+    return ifSuccess ? R.Ok().Data(res) : R.Fail().Msg(res.message)
   }
 
   @Post('/render')
@@ -35,51 +35,51 @@ export class TemplateController {
     const [filename, filepath] = await this._templateService.renderAndBuild(
       data.fid,
       data.tags
-    );
+    )
     // ctx.attachment([filename], [options]) 将 Content-Disposition 设置为 “附件” 以指示客户端提示下载。
     ctx.attachment(filename, {
       fallback: true,
       type: 'attachment', // [string] attachment/inline
-    });
-    const stats = statSync(filepath);
+    })
+    const stats = statSync(filepath)
     ctx.response.set({
       'Content-Type': 'application/octet-stream',
       'Content-Disposition': `attachment; filename=${filename}`,
       'Content-Length': stats.size.toString(),
-    });
-    ctx.body = createReadStream(filepath);
+    })
+    ctx.body = createReadStream(filepath)
   }
 
   @Get('/')
   async list(): Promise<R> {
-    return R.Ok().Data(await this._templateService.getTemplateList());
+    return R.Ok().Data(await this._templateService.getTemplateList())
   }
 
   @Get('/:fid')
   async getTemplate(ctx: Context, @Param('fid') fid: string) {
-    const [filename, filepath] = await this._templateService.getTemplate(fid);
+    const [filename, filepath] = await this._templateService.getTemplate(fid)
     // ctx.attachment([filename], [options]) 将 Content-Disposition 设置为 “附件” 以指示客户端提示下载。
     ctx.attachment(filename, {
       fallback: true,
       type: 'attachment', // [string] attachment/inline
-    });
-    const stats = statSync(filepath);
+    })
+    const stats = statSync(filepath)
     ctx.response.set({
       'Content-Type': 'application/octet-stream',
       'Content-Disposition': `attachment; filename=${filename}`,
       'Content-Length': stats.size.toString(),
-    });
-    ctx.body = createReadStream(filepath);
+    })
+    ctx.body = createReadStream(filepath)
   }
 
   @Del('/:fid')
   async deleteTemplate(@Param('fid') fid: string): Promise<R> {
-    const ifSuccess = await this._templateService.deleteTemplate(fid);
-    return ifSuccess ? R.Ok() : R.Fail();
+    const ifSuccess = await this._templateService.deleteTemplate(fid)
+    return ifSuccess ? R.Ok() : R.Fail()
   }
 
   @Get('/tags')
   async getTags(@Query('fid') fid: string): Promise<R> {
-    return R.Ok().Data(await this._templateService.getTags(fid));
+    return R.Ok().Data(await this._templateService.getTags(fid))
   }
 }
