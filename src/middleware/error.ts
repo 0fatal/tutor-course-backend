c76154cdcbee0c52073bebf9bbbc57c5b725c693
ZacharyWin <2816813070@qq.com>
2021-12-01 19:17:01 +0800

    feat(middleware): error

---
+++ src/middleware/error.ts
@@ -0,0 +1,28 @@
+import { IMidwayWebNext, IWebMiddleware } from '@midwayjs/web'
+import { Context } from 'egg'
+import { Provide } from '@midwayjs/decorator'
+import { R } from '../utils/response'
+
+@Provide()
+export class ErrorMiddleware implements IWebMiddleware {
+  resolve() {
+    return async (ctx: Context, next: IMidwayWebNext) => {
+      try {
+        await next()
+      } catch (err) {
+        console.log(err.details)
+        if (err?.details?.[0].type === 'any.required') {
+          ctx.status = 200
+          ctx.body = R.Fail().Msg(err.message)
+        } else {
+          ctx.body = {
+            message: err.message,
+            code: err.code,
+            stack: err.stack,
+          }
+          ctx.status = err.status || 500
+        }
+      }
+    }
+  }
+}
