232b02353aff3feb316db79136919886a0fd49f7
ZacharyWin <2816813070@qq.com>
2021-12-01 19:14:06 +0800

    feat(service): teacher

--- src/service/teacher.ts
+++ src/service/teacher.ts
@@ -2,8 +2,11 @@ import { InjectEntityModel } from '@midwayjs/orm'
 import { Teacher } from '../entity/teacher'
 import { Repository } from 'typeorm'
 import MD5 from 'crypto-js/md5'
-import { RegistryTeacherDTO } from '../dto/teacher/teacher'
+import { RegistryTeacherDTO, UpdateTeacherDTO } from '../dto/teacher/teacher'
+import { Provide } from '@midwayjs/decorator'
+import { TeacherInfoDTO } from '../dto/teacher/teacher.response'
 
+@Provide()
 export class TeacherService {
   @InjectEntityModel(Teacher)
   _teacherModel: Repository<Teacher>
@@ -21,24 +24,41 @@ export class TeacherService {
     })
   }
 
-  async findByStaffId(staffId: string): Promise<Teacher> {
-    return await this._teacherModel.findOne(staffId)
+  async findByStaffId(staffId: string): Promise<TeacherInfoDTO> {
+    const teacher: TeacherInfoDTO = (await this._teacherModel.findOne(staffId, {
+      select: ['staffId', 'name', 'isAdmin', 'createAt', 'updateAt'],
+    })) as TeacherInfoDTO
+    return teacher
   }
 
-  async registry(info: RegistryTeacherDTO): Promise<boolean> {
-    // eslint-disable-next-line prefer-const
-    let { staffId, password, name } = info
-    password = MD5(password).toString()
-
-    if (
-      !(await this._teacherModel.create({
+  async registry(info: RegistryTeacherDTO): Promise<[boolean, any]> {
+    try {
+      // eslint-disable-next-line prefer-const
+      let { staffId, password, name } = info
+      password = MD5(password).toString()
+      const teacher = this._teacherModel.create({
         staffId,
         password,
         name,
-      }))
-    ) {
+      })
+
+      await this._teacherModel.insert(teacher)
+      return [true, null]
+    } catch (e: any) {
+      console.log(e)
+      const message: string =
+        e.code === 'ER_DUP_ENTRY' ? 'teacher exists' : null
+      return [false, message]
+    }
+  }
+
+  async updateInfo(staffId: string, data: UpdateTeacherDTO): Promise<boolean> {
+    try {
+      const res = await this._teacherModel.update(staffId, data)
+      return res.affected === 1
+    } catch (e: any) {
+      console.log(e)
       return false
     }
-    return true
   }
 }
