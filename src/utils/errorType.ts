140dc1a308c0ecaa23c2df31dc97d9f2071d83ef
ZacharyWin <2816813070@qq.com>
2021-12-01 19:17:28 +0800

    feat(util): errorType

--- src/utils/errorType.ts
+++ src/utils/errorType.ts
@@ -2,13 +2,14 @@ export enum ErrorType {
   REQUEST_FORBIDDEN = 0,
   SERVER_ERROR,
   UNAUTHORIZED,
+  LOGIN_FAILED,
 }
 
 export const ErrorMap: {
   [key in ErrorType]: {
-    code: number;
-    msg: string;
-  };
+    code: number
+    msg: string
+  }
 } = {
   [ErrorType.REQUEST_FORBIDDEN]: {
     code: 40300,
@@ -22,4 +23,8 @@ export const ErrorMap: {
     code: 40100,
     msg: 'unauthorized',
   },
-};
+  [ErrorType.LOGIN_FAILED]: {
+    code: 40201,
+    msg: 'wrong staffId or password',
+  },
+}
