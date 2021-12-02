1f5a49ecfe2cc0c5a85a31cee403c149adb34c12
ZacharyWin <2816813070@qq.com>
2021-12-02 16:16:02 +0800

    change(utils): response

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,9 @@
 import { IMidwayWebNext, IWebMiddleware } from '@midwayjs/web'
 import { Context } from 'egg'
 import { Teacher } from '../entity/teacher'
-import { ErrorType } from '../utils/errorType'
 import { R } from '../utils/response'
 import { Provide } from '@midwayjs/decorator'
+import { ErrorMap } from '../errorType/base'
 
 @Provide()
 export class AuthMiddleware implements IWebMiddleware {
@@ -13,7 +13,7 @@ export class AuthMiddleware implements IWebMiddleware {
         return await next()
       const teacher = ctx.session.teacher as Teacher
       if (!teacher) {
-        ctx.body = R.WrapError(ErrorType.UNAUTHORIZED)
+        ctx.body = R.WrapError(ErrorMap['UNAUTHORIZED'])
         ctx.status = 200
         return
       }
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,41 +1,40 @@
-import { ErrorMap, ErrorType } from './errorType';
+import { ErrorDefineSingle } from '../errorType/base'
 
 export class R {
-  code: number;
-  msg: string;
-  data = null;
+  code: number
+  msg: string
+  data = null
 
   constructor(code: number, msg: string, data: any) {
-    this.code = code;
-    this.msg = msg;
-    this.data = data;
+    this.code = code
+    this.msg = msg
+    this.data = data
   }
 
   Code(code: number): R {
-    this.code = code;
-    return this;
+    this.code = code
+    return this
   }
 
   Data(data: any): R {
-    this.data = data;
-    return this;
+    this.data = data
+    return this
   }
 
   Msg(msg: string): R {
-    this.msg = msg;
-    return this;
+    this.msg = msg
+    return this
   }
 
   static Ok(): R {
-    return new R(0, 'success', null);
+    return new R(0, 'success', null)
   }
 
   static Fail(): R {
-    return new R(-1, 'fail', null);
+    return new R(-1, 'fail', null)
   }
 
-  static WrapError(errorType: ErrorType): R {
-    const err = ErrorMap[errorType];
-    return this.Fail().Code(err.code).Msg(err.msg);
+  static WrapError(err: ErrorDefineSingle): R {
+    return this.Fail().Code(err.code).Msg(err.msg)
   }
 }
