fe743a96a3d3f88cd1b7d4af2b4e435df2c8d81f
ZacharyWin <2816813070@qq.com>
2021-12-16 18:53:49 +0800

    lint

--- src/utils/doc.ts
+++ src/utils/doc.ts
@@ -1,20 +1,20 @@
-import Docxtemplater from 'docxtemplater';
-import InspectModule from 'docxtemplater/js/inspect-module';
+import Docxtemplater from 'docxtemplater'
+import InspectModule from 'docxtemplater/js/inspect-module'
 import PizZip = require('pizzip')
 
 // import PizZipUtils from 'pizzip/utils'
 
 export function getDocxTags(doc: Docxtemplater) {
-  const iModule = new InspectModule();
-  doc.attachModule(iModule);
-  doc.render();
-  const tags = iModule.getAllTags();
-  console.log(tags);
-  return tags;
+  const iModule = new InspectModule()
+  doc.attachModule(iModule)
+  doc.render()
+  const tags = iModule.getAllTags()
+  console.log(tags)
+  return tags
 }
 
 export function loadDocxFile(data): Docxtemplater {
-  const zip = new PizZip(data); // 将内容转化PizZip对象
+  const zip = new PizZip(data) // 将内容转化PizZip对象
 
   const doc = new Docxtemplater().loadZip(zip)
   return doc
