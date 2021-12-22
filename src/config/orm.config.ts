753dd19e645497de4ecfabb1bc63c2da7339c25f
ZacharyWin <2816813070@qq.com>
2021-12-22 15:07:05 +0800

    fix

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -3,7 +3,7 @@ export const orm = {
   host: '172.18.0.1',
   port: 3306,
   username: 'course_manager',
-  password: 'hJ68WPMhCaK7BfeTr',
+  password: 'J68WPMhCaK7BfeTr',
   database: 'course_home',
   synchronize: false, // 如果第一次使用，不存在表，有同步的需求可以写 true
   logging: false,
