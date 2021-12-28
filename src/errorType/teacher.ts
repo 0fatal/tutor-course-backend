2bd49fa191bcdf293edfb8c9299eac1201b0b85d
ZacharyWin <2816813070@qq.com>
2021-12-29 00:47:03 +0800

    update: teacher

--- src/errorType/teacher.ts
+++ src/errorType/teacher.ts
@@ -3,6 +3,8 @@ import { ErrorDefine } from './base'
 export interface ErrorType {
   teacher: {
     LOGIN_FAILED: {}
+    LOGIN_FORBIDDEN: {}
+    NOT_FOUND: {}
   }
 }
 
@@ -10,5 +12,13 @@ export const TeacherErrorMap: ErrorDefine<'teacher', ErrorType> = {
   LOGIN_FAILED: {
     code: 400101,
     msg: '工号或密码错误',
+  },
+  LOGIN_FORBIDDEN: {
+    code: 400102,
+    msg: '该账号已被禁止登录',
+  },
+  NOT_FOUND: {
+    code: 400103,
+    msg: '该账号不存在',
   },
 }
