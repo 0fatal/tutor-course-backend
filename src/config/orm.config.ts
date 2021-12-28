4f75185f7bea1670805c3ee6710fabfe38ac2724
ZacharyWin <2816813070@qq.com>
2021-12-28 13:29:31 +0800

    update: config

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,4 +1,4 @@
-export const orm = {
+const prod = {
   type: 'mysql',
   host: '172.18.0.1',
   port: 3306,
@@ -8,3 +8,16 @@ export const orm = {
   synchronize: false, // 如果第一次使用，不存在表，有同步的需求可以写 true
   logging: false,
 }
+
+const dev = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'hifffff123',
+  database: 'course_home',
+  synchronize: false, // 如果第一次使用，不存在表，有同步的需求可以写 true
+  logging: false,
+}
+
+export const orm = process.env.NODE_ENV === 'production' ? prod : dev
